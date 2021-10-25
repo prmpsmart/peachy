c831ea9d198c6aed690fd723dbdf0117f6502693
prmpsmart <prmpsmart@gmail.com>
2021-10-25 13:14:07 +0100

    wanna reset to normal

--- android/app/src/main/java/com/peachy/MainActivity.java
+++ android/app/src/main/java/com/peachy/MainActivity.java
@@ -1,15 +1,6 @@
 package com.peachy;
 
 import io.flutter.embedding.android.FlutterActivity;
-import android.os.Bundle;
 
 public class MainActivity extends FlutterActivity { 
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        getWindow().setStatusBarColor(Color.parseColor("#fff9eb"));
-        // getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-    }
 }
